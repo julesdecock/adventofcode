028990c2d70ccb71d46acd5c1360cead8c35aa1e
decocju <decocju@cronos.be>
2022-12-06 21:18:05 +0100

    Day 1 solution part 2

--- Day 1/CountCalories.py
+++ Day 1/CountCalories.py
@@ -1,4 +1,3 @@
-# Part 1
 elves = []
 elve = []
 
@@ -9,5 +8,14 @@ for line in open('input.txt').read().split('\n'):
         continue
     elve.append(int(line))
 
-mostCalories = max(sum(cal) for cal in elves)    
-print(mostCalories)
+allElvesSum = []
+for cal in elves:
+    allElvesSum.append(sum(cal))
+
+allElvesSum.sort(reverse=True)
+
+# Solution part 1
+print('Solution part 1: ' + str(max(sum(cal) for cal in elves)))
+
+# Solution part 2
+print('Solution part 2: ' + str(sum(allElvesSum[:3])))
